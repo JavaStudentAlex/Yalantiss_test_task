bbb421dd161ec9de43fe5a0454544d09f28fbd06
@lex Tarasov <tarasov.oleksander@gmail.com>
2021-05-02 21:14:43 +0300

    Create flask app from config object.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 """Module for initialising  flask application"""
-from datetime import date
+from datetime import datetime
 from os import environ
 
 from flask import Flask
@@ -10,34 +10,56 @@ from app.models import Course, bcrypt, db
 from app.swagger import SWAGGER_UI_BLUEPRINT, SWAGGER_URL
 
 
-def __add_default_course(app: Flask):
+def __add_default_courses(app: Flask):
     app.app_context().push()
     db.create_all()
     if Course.query.count() == 0:
-        course = Course("course", date.today(), date.today(), 1)
+
+        course = Course(
+            "course1",
+            datetime.strptime("2021-01-01", "%Y-%m-%d").date(),
+            datetime.strptime("2021-01-01", "%Y-%m-%d").date(),
+            1,
+        )
         db.session.add(course)
+
+        course2 = Course(
+            "course2",
+            datetime.strptime("2019-01-01", "%Y-%m-%d").date(),
+            datetime.strptime("2019-01-01", "%Y-%m-%d").date(),
+            2,
+        )
+        db.session.add(course2)
+
+        course3 = Course(
+            "course3",
+            datetime.strptime("2018-01-01", "%Y-%m-%d").date(),
+            datetime.strptime("2018-01-01", "%Y-%m-%d").date(),
+            3,
+        )
+        db.session.add(course3)
+
         db.session.commit()
 
 
-def create_flask_app():
+def create_flask_app(config_object=None):
     """Flask app factory method"""
     app = Flask(__name__)
-    app.config.from_mapping(
-        BCRYPT_LOG_ROUNDS=10,
-        SECRET_KEY=environ["FLASK_SECRET_KEY"],
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        SQLALCHEMY_DATABASE_URI=environ["DATABASE_PATH"],
-    )
-
-    @app.route("/")
-    def hello_world():
-        return "Hello World!"
+    if config_object is not None:
+        app.config.from_object(config_object)
+    else:
+        app.config.from_mapping(
+            BCRYPT_LOG_ROUNDS=10,
+            SECRET_KEY=environ["FLASK_SECRET_KEY"],
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            SQLALCHEMY_DATABASE_URI=environ["DATABASE_PATH"],
+        )
 
     # init database
     db.init_app(app)
     bcrypt.init_app(app)
     Migrate(app, db)
-    __add_default_course(app)
+    __add_default_courses(app)
 
     # init api
     api.init_app(app)
